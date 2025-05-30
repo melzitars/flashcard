29e1861a2ef28b03a6c348a2fe0ffc52309fab46
Melanie <melaine.souza@escola.pr.gov.br>
2025-05-30 00:13:40 +0000

    java

---
+++ perguntas.js
@@ -0,0 +1,59 @@
+criaCartao(
+    'Psiquiatria',
+    'O que é psiquiatria?',
+    'A psiquiatria é uma área médica focada no estudo, diagnóstico, tratamento e prevenção de doenças mentais.'
+)
+
+criaCartao(
+    'Psiquiatria',
+    'O que a psiquiatria trata?',
+    'Distúrbios psicológicos.'
+)
+
+criaCartao(
+    'Psiquiatria',
+    'Quem precisa ir em consultas psiquiatricas?',
+    'Todos'
+)
+
+criaCartao(
+    'Medicina',
+    'Qual a área da medicina que trata saúde mental?',
+    'Psiquiatria.'
+)
+
+criaCartao(
+    'Medicina',
+    'Médicos aplicam injeção?',
+    'Sim, aplicam.'
+)
+
+criaCartao(
+    'Medicina',
+    'Qual o nome do médico que cuida do estômago?',
+    'O gastroenterologista.'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual o osso mais resistente do corpo humano?',
+    'Fêmur.'
+)
+
+criaCartao(
+    'Biotecnologia',
+    'O que a vacina faz no corpo?',
+    'Estimula a criação de anticorpos.'
+)
+
+criaCartao(
+    'Medicina',
+    'O que a diabetes faz?',
+    'Cria uma deficiência na produção de insulina.'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual é o meio de respiração da célula?',
+    'Mitocôndria'
+)
